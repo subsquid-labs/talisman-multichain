5be26f0be5ad3961d49b83536cc5c8fd0e81be00
Mignight Swami <daniel@talisman.xyz>
2022-06-22 17:11:29 +0200

    remove dependency test - reverted

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,9 @@
-//import { encodeAddress } from '@polkadot/util-crypto'
+import { encodeAddress } from '@polkadot/util-crypto'
 
 // substrate format
-//const ss58Format = 42
+const ss58Format = 42
 
 // encode, lowercase and return any address by type and format
 export const formatAddress = (address: string | Uint8Array) => {
-  //return encodeAddress(address, ss58Format)
-  return address as string
+  return encodeAddress(address, ss58Format)
 }
