ea759e6514c23edb2c1b20563298e630e120c6c5
Mignight Swami <daniel@talisman.xyz>
2022-06-22 14:44:19 +0200

    test

--- db/migrations/1655901786536-Init.js
+++ db/migrations/1655901786536-Init.js
@@ -1,5 +1,5 @@
-module.exports = class Init1655880702633 {
-  name = 'Init1655880702633'
+module.exports = class Init1655901786536 {
+  name = 'Init1655901786536'
 
   async up(db) {
     await db.query(`CREATE TABLE "chain" ("id" character varying NOT NULL, "url" text NOT NULL, "starting_block" numeric NOT NULL, "latest_block" numeric NOT NULL, "hash" text NOT NULL, "ss58_format" integer NOT NULL, "created_at" numeric NOT NULL, "updated_at" numeric NOT NULL, CONSTRAINT "PK_8e273aafae283b886672c952ecd" PRIMARY KEY ("id"))`)
